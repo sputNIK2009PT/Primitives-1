7dd15d3b77675e3c733e8d1886328848683d30dc
Alexander Stupnikov <iam-not@mail.ru>
2024-06-02 16:25:58 +0300

    Initail commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13676;
+        int rublesPerMile = 20;
+        int bonusMiles = ticketPrice / rublesPerMile;
+        System.out.println(bonusMiles + " бонусных миль.");
+    }
+}
